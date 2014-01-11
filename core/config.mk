818d1d631dc41b74a9f3c235c4b1fa26322d2296
Humberto Borba <humberos@gmail.com>
2014-01-11 21:49:49 -0200

    sepolicy: add aosip common selinux policy inclusion
    It is required for https://gerrit.omnirom.org/#/c/4563
    
    Change-Id: I22c47febefd986274b53d69f89df6bff589c8ea0
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -915,9 +915,11 @@ ifneq ($(TARGET_COPY_FILES_OVERRIDES),)
     PRODUCT_COPY_FILES := $(filter-out $(TARGET_COPY_FILES_OVERRIDES), $(PRODUCT_COPY_FILES))
 endif
 
+ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/aosp/sepolicy/sepolicy.mk)
+$(eval include vendor/aosip/sepolicy/sepolicy.mk)
+endif
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
