iskandar1023/build

Add global vendor selinux policy (1/2)
We're adding specific policies to an overlay instead of patching
external/sepolicy, so we need to make sure it's included to prevent
misbehaving systems

Needs another commit on vendor/krexus

core/config.mk
@@ -689,4 +689,8 @@ RSCOMPAT_NO_USAGEIO_API_LEVELS := 8 9 10 11 12 13
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include vendor/krexus/sepolicy/sepolicy.mk)
+
 include $(BUILD_SYSTEM)/dumpvar.mk

xlxfoxxlx/platform_build

Conflicts:
	core/config.mk
Change-Id: Iaa35ab520edbde11e6cd34f9d38d11a1594b07bd

core/config.mk
@@ -692,4 +692,8 @@ endif
 RSCOMPAT_32BIT_ONLY_API_LEVELS := 8 9 10 11 12 13 14 15 16 17 18 19 20
 RSCOMPAT_NO_USAGEIO_API_LEVELS := 8 9 10 11 12 13
 
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include vendor/aosip/sepolicy/sepolicy.mk)
+
 include $(BUILD_SYSTEM)/dumpvar.mk