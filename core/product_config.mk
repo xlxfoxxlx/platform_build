739042e9651898ea1afc4335c19572f34ab6af27
Ricardo Cerqueira <github@cerqueira.org>
2017-05-02 15:09:24 +0200

    build: Preliminary support for AOSiP product builds
    If building a aosip_* product, skip crawling the filesystem for
    AndroidProduct makefiles (and, of course, parsing their contents)
    and aim directly for the device/*/<product> device configuration
    
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    
    Change how we search for AOSiP makefiles.
    
    build: Fix fastpath code for AOSIP_BUILD
    
     * We need to adjust to the new validations
    
    Change-Id: I9066d4f437beb9597027ee6bbb52504b5e7e84c5
    
    build: Fix aosip builds
    
    the setup variable wasn't exported, and was being missed by part
    of the scripts
    
    AOSiP- We already had a part of this squashed in somewhere else, added the remnants
    
    Change-Id: I0d23341d24c56bcbbf9e3799aa587172b3eb0075

--- core/product_config.mk
+++ core/product_config.mk
@@ -194,6 +194,7 @@ ifneq ($(CUSTOM_BUILD),)
   endif
  endif
 
+ifneq ($(CUSTOM_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
@@ -215,6 +216,11 @@ $(foreach f, $(all_product_configs),\
 _cpm_words :=
 _cpm_word1 :=
 _cpm_word2 :=
+else
+    current_product_makefile := $(strip $(all_product_configs))
+    all_product_makefiles := $(strip $(all_product_configs))
+endif
+
 current_product_makefile := $(strip $(current_product_makefile))
 all_product_makefiles := $(strip $(all_product_makefiles))
 
